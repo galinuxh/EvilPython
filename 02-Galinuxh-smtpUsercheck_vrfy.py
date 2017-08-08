2c90d7744877cbbe98bee25a0c7a6b5cc3adf430
root <root@localhost.localdomain>
2017-08-08 13:29:59 -0400

    Evil Python

--- 02-Galinuxh-smtpUsercheck_vrfy.py
+++ 02-Galinuxh-smtpUsercheck_vrfy.py
@@ -11,8 +11,8 @@ argumentos = argparse.ArgumentParser(description="Enumeracion de usuarios por me
 argumentos.add_argument("-t","--target",help="direccion ip del target a validar",dest="host",required=True)
 argumentos.add_argument("-p","--port",help="Numero del puerto del servicio a validar",dest="port",type=int,default=25)
 argumentos.add_argument("-u","--userlist",help="Archivo de usuarios",dest="userlist",required=True)
+argumentos.add_argument("-o","--output",help="Guardar Resultados",dest="output",required=False,default=" ")
 argumentos.add_argument("-v","--vervose",help="Salida con intentos  fallidos",dest="vervose",action="store_true",default=False)
-
 
 arg = argumentos.parse_args()#
 
@@ -22,13 +22,16 @@ def banner ():
 
     evilbanner = """
 
- ██████╗  █████╗ ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗
-██╔════╝ ██╔══██╗██║     ██║████╗  ██║██║   ██║╚██╗██╔╝
-██║  ███╗███████║██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝
-██║   ██║██╔══██║██║     ██║██║╚██╗██║██║   ██║ ██╔██╗
-╚██████╔╝██║  ██║███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗
- ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝
-----------------------------------------------------------
+
+  /$$$$$$            /$$ /$$                               /$$   /$$
+ /$$__  $$          | $$|__/                              | $$  | $$
+| $$  \__/  /$$$$$$ | $$ /$$ /$$$$$$$  /$$   /$$ /$$   /$$| $$  | $$
+| $$ /$$$$ |____  $$| $$| $$| $$__  $$| $$  | $$|  $$ /$$/| $$$$$$$$
+| $$|_  $$  /$$$$$$$| $$| $$| $$  \ $$| $$  | $$ \  $$$$/ | $$__  $$
+| $$  \ $$ /$$__  $$| $$| $$| $$  | $$| $$  | $$  >$$  $$ | $$  | $$
+|  $$$$$$/|  $$$$$$$| $$| $$| $$  | $$|  $$$$$$/ /$$/\  $$| $$  | $$
+ \______/  \_______/|__/|__/|__/  |__/ \______/ |__/  \__/|__/  |__/
+--------------------------------------------------------------------
 EvilPython : Enumeracion de Usarios por SMTP - VRFY
 Autor      : José Luis Galindo Herrera
 email      : galinuxh@gmail.com..
@@ -36,18 +39,21 @@ github     : https://github.com/galinuxh/EvilPython/
     """
     print(evilbanner)
 
-
-def bruteforceSMTP(hosts,port,userlist):
+output = ""
+def bruteforceSMTP(hosts,port,userlist,output):
         print("host :"+hosts+" puerto: "+str(port))
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((hosts,port))
-        datos = s.recv(1024)
+
         usercheck = open(userlist,"r")
-        print("Banner recibido del servidor remoto")
-        print(datos)
+
+        if arg.output != " ":
+            save = open(output,"w")
+
         try:
-            #s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            #s.connect((hosts,port))
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((hosts,port))
+            datos = s.recv(1024)
+            print("Banner recibido del servidor remoto")
+            print(datos)
             for user in usercheck:
                 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                 s.connect((hosts,port))
@@ -58,14 +64,20 @@ def bruteforceSMTP(hosts,port,userlist):
                 #print(check)
                 if ("252") in check:
                     print("[+] Usuario Activo   : "+user)
+                    if arg.output != " ":
+                        save.write(user)
                 elif arg.vervose == True:
                     print("[-] Usuario Inactivo : "+user)
                 else:
                     s.close()
                     pass
+            if arg.output != " ":
+                save.close()
+            usercheck.close()
 
-        except:
-            print("Error  A conectar con el servidor")
-            pass
+        except socket.error, msg: # manejo de errores de conexion con socket
+            print("Error al conectar con el servidor :"+ str(msg))
+            sys.exit(1)
+
 banner()
-bruteforceSMTP(arg.host,arg.port,arg.userlist)
+bruteforceSMTP(arg.host,arg.port,arg.userlist,arg.output)
